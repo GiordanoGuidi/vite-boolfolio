ca09fe7e64886b2ccad6034294aef8ddcd1fd3cf
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-29 16:14:47 +0100

    added AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,19 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader }
+}
 </script>
+
+<template>
+  <main>
+    <AppHeader />
+    <div class="container pt-4">
+      <h1>Boolfolio</h1>
+
+    </div>
+  </main>
+</template>
+
+<style></style>
