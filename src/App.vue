aa98b8a4de6870b5e047e94ce7c898b26224beed
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-29 16:42:38 +0100

    printed projects dynamically in page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <script>
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects';
 export default {
   name: 'Boolfolio',
-  components: { AppHeader }
-}
+  components: { AppHeader },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+        this.projects = res.data
+      })
+    }
+  },
+  created() {
+    this.fetchProjects()
+  }
+};
 </script>
 
 <template>
+  <AppHeader />
   <main>
-    <AppHeader />
     <div class="container pt-4">
       <h1>Boolfolio</h1>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          <h3>{{ project.title }}</h3>
+        </li>
+      </ul>
 
     </div>
   </main>
