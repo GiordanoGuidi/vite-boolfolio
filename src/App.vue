2590860b6a65c215c1194d2f73dee82efa46d8aa
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-30 20:56:21 +0100

    added component AppAlert

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,19 @@ import ProjectsList from './components/projects/ProjectsList.vue';
 import ProjectCard from './components/projects/ProjectCard.vue';
 import BasePagination from './components/BasePagination.vue';
 import AppLoader from './components/AppLoader.vue';
+import AppAlert from './components/AppAlert.vue';
 import axios from 'axios';
 const deafaultEndpoint = 'http://localhost:8000/api/projects';
 export default {
   name: 'Boolfolio',
-  components: { AppHeader, ProjectsList, ProjectCard, BasePagination, AppLoader },
+  components: { AppHeader, ProjectsList, ProjectCard, BasePagination, AppLoader, AppAlert },
   data: () => ({
     projects: {
       data: [],
       links: [],
     },
     isLoading: false,
+    isAlertOpen: false,
   }),
   methods: {
     fetchProjects(endpoint) {
@@ -22,11 +24,18 @@ export default {
       axios.get(endpoint ?? deafaultEndpoint).then(res => {
         const { data, links } = res.data;
         this.projects = { data, links };
+        this.isAlertOpen = false;
       }).catch(err => {
         console.error(err);
+        this.isAlertOpen = true;
       }).then(() => {
         this.isLoading = false;
       })
+    },
+
+    closeAlert() {
+      const alert = document.querySelector('.alert')
+      alert.classList.add('d-none')
     }
   },
   created() {
@@ -40,6 +49,7 @@ export default {
   <main>
     <div class="container pt-4">
       <h1>Boolfolio</h1>
+      <AppAlert v-if="isAlertOpen" @close-button="isAlertOpen = false" @retry="fetchProjects" />
       <AppLoader v-if="isLoading" />
       <ProjectsList v-else :projects="projects.data" />
       <BasePagination :links="projects.links" @change-page="fetchProjects" />
--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,11 +1,17 @@
 <script>
 export default {
     name: 'AppAlert',
+    emit: ['close-button', 'retry'],
 };
 </script>
 
 <template>
-
+    <div class="alert alert-warning alert-dismissible fade show mt-3" role="alert">
+        Si è verificato un errore. <span role="button" @click="$emit('retry')"
+            class="text-decoration-underline">Riprova</span>
+        <button type="button" @click="$emit('close-button')" class="btn-close" data-bs-dismiss="alert"
+            aria-label="Close"></button>
+    </div>
 </template>
 
 <style></style>
