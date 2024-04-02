df7d017109267bd5a653dae90dd0bb255fa3f0c4
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-02 18:21:29 +0200

    added route project-detail

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 //Importo le pagine
 import HomePage from '../pages/HomePage.vue';
+import ProjectDetail from '../pages/ProjectDetail.vue';
 //Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', name: 'home', component: HomePage }
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/projects/:id', name: 'project-detail', component: ProjectDetail }
         //#Aggiungi pagina Not Found
     ]
 });
