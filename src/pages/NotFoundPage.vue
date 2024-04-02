22e24586ca2060cfd93b8ceffb14dd76cc1fdeac
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-02 22:05:19 +0200

    added component NotFounPage into pages

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+}
+</script>
+
+<template>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <h3> 404 | NotFounPage</h3>
+    </div>
+</template>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 //Importo le pagine
 import HomePage from '../pages/HomePage.vue';
 import ProjectDetail from '../pages/ProjectDetail.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 //Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'home', component: HomePage },
-        { path: '/projects/:slug', name: 'project-detail', component: ProjectDetail }
+        { path: '/projects/:slug', name: 'project-detail', component: ProjectDetail },
+        { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage },
+
         //#Aggiungi pagina Not Found
     ]
 });
