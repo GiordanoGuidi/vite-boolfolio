e281b381bb7c8b692afd59262dd088fae5b74366
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-02 16:59:52 +0200

    filled file index.js into router

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWenHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage,
+        }
+    ]
+});
+export { router };
