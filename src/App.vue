57cc26c53e9c7375ea1916ca935870c73a39b194
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-02 16:30:52 +0200

    added file pages and component HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,9 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
-import ProjectCard from './components/projects/ProjectCard.vue';
-import BasePagination from './components/BasePagination.vue';
-import AppLoader from './components/AppLoader.vue';
-import AppAlert from './components/AppAlert.vue';
-import axios from 'axios';
-const deafaultEndpoint = 'http://localhost:8000/api/projects';
+import HomePage from './pages/HomePage.vue';
 export default {
   name: 'Boolfolio',
-  components: { AppHeader, ProjectsList, ProjectCard, BasePagination, AppLoader, AppAlert },
-  data: () => ({
-    projects: {
-      data: [],
-      links: [],
-    },
-    isLoading: false,
-    isAlertOpen: false,
-  }),
-  methods: {
-    fetchProjects(endpoint) {
-      this.isLoading = true;
-      axios.get(endpoint ?? deafaultEndpoint).then(res => {
-        const { data, links } = res.data;
-        this.projects = { data, links };
-        this.isAlertOpen = false;
-      }).catch(err => {
-        console.error(err);
-        this.isAlertOpen = true;
-      }).then(() => {
-        this.isLoading = false;
-      })
-    },
-
-    closeAlert() {
-      const alert = document.querySelector('.alert')
-      alert.classList.add('d-none')
-    }
-  },
-  created() {
-    this.fetchProjects()
-  }
+  components: { AppHeader, HomePage },
 };
 </script>
 
@@ -48,11 +11,7 @@ export default {
   <AppHeader />
   <main>
     <div class="container pt-4">
-      <h1>Boolfolio</h1>
-      <AppAlert v-if="isAlertOpen" @close-button="isAlertOpen = false" @retry="fetchProjects" />
-      <AppLoader v-if="isLoading" />
-      <ProjectsList v-else :projects="projects.data" />
-      <BasePagination :links="projects.links" @change-page="fetchProjects" />
+      <HomePage />
     </div>
   </main>
 </template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,54 @@
+<script>
+import ProjectsList from '../components/projects/ProjectsList.vue';
+import ProjectCard from '../components/projects/ProjectCard.vue';
+import BasePagination from '../components/BasePagination.vue';
+import AppLoader from '../components/AppLoader.vue';
+import AppAlert from '../components/AppAlert.vue';
+import axios from 'axios';
+const deafaultEndpoint = 'http://localhost:8000/api/projects';
+export default {
+    name: 'HomePage',
+    components: { ProjectsList, ProjectCard, BasePagination, AppLoader, AppAlert },
+    data: () => ({
+        projects: {
+            data: [],
+            links: [],
+        },
+        isLoading: false,
+        isAlertOpen: false,
+    }),
+    methods: {
+        fetchProjects(endpoint) {
+            this.isLoading = true;
+            axios.get(endpoint ?? deafaultEndpoint).then(res => {
+                const { data, links } = res.data;
+                this.projects = { data, links };
+                this.isAlertOpen = false;
+            }).catch(err => {
+                console.error(err);
+                this.isAlertOpen = true;
+            }).then(() => {
+                this.isLoading = false;
+            })
+        },
+
+        closeAlert() {
+            const alert = document.querySelector('.alert')
+            alert.classList.add('d-none')
+        }
+    },
+    created() {
+        this.fetchProjects()
+    }
+};
+</script>
+
+<template>
+    <h1>Boolfolio</h1>
+    <AppAlert v-if="isAlertOpen" @close-button="isAlertOpen = false" @retry="fetchProjects" />
+    <AppLoader v-if="isLoading" />
+    <ProjectsList v-else :projects="projects.data" />
+    <BasePagination v-if="!isLoading" :links="projects.links" @change-page="fetchProjects" />
+</template>
+
+<style></style>
