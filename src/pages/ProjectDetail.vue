67b5b74484ba3c56ee0ff6944716860165ae57f5
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-02 18:21:48 +0200

    added component ProjectDetail

---
+++ src/pages/ProjectDetail.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'ProjectDetail',
+};
+</script>
+
+<template>
+    <!-- <div class="col-md-8">
+        <div class="card-header">
+            <h5 class="card-title">{{ project.title }}</h5>
+        </div>
+        <div class="card-body">
+            <img v-if="project.image" :src="project.image" class="float-start img-fluid" :alt="project.title">
+            <p>{{ abstract }}</p>
+        </div>
+        <div class="card-footer">
+            <address class="card-text">{{ project.content }}</address>
+            <small>Pubblicato il: {{ formattedDate }}</small>
+        </div>
+    </div> -->
+    <h2>Ciao</h2>
+</template>
+
+<style></style>
