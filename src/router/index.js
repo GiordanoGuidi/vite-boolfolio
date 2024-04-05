1ecc412ae4f11677cbb2264b91f959fef4ed563d
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-05 19:24:42 +0200

    added path technology-projects-page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import HomePage from '../pages/HomePage.vue';
 import ProjectDetail from '../pages/ProjectDetail.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
 import TypeProjectsPage from '../pages/TypeProjectsPage.vue';
+import TechnologyProjectsPage from '../pages/TechnologyProjectsPage.vue';
 //Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
@@ -17,6 +18,7 @@ const router = createRouter({
         { path: '/not-found', name: 'not-found', component: NotFoundPage },
         { path: '/:pathMatch(.*)*', redirect: 'not-found' },
         { path: '/types/:slug/projects', name: 'type-projects-page', component: TypeProjectsPage },
+        { path: '/technologies/:slug/projects', name: 'technology-projects-page', component: TechnologyProjectsPage }
     ]
 });
 export { router };
