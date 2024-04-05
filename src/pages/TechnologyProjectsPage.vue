897267c0c1289df36d96c36c4cddf33fd58e1cf4
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-05 19:59:52 +0200

    added watch

--- src/pages/TechnologyProjectsPage.vue
+++ src/pages/TechnologyProjectsPage.vue
@@ -32,6 +32,13 @@ export default {
     },
     created() {
         this.fetchProjects();
+    },
+    watch: {
+        '$route'(to, from) {
+            if (to.params.slug !== from.params.slug) {
+                this.fetchProjects();
+            }
+        }
     }
 };
 </script>
