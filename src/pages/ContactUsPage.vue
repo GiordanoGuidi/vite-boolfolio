8d5c2c07a8531c0133238782c269e4644be345d9
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-12 18:32:02 +0200

    added form into ContactUsPage

--- src/pages/ContactUsPage.vue
+++ src/pages/ContactUsPage.vue
@@ -2,12 +2,41 @@
 // import { store } from './data/store';
 export default {
     name: 'ContactUsPage',
-
 };
 </script>
 
 <template>
     <h1>Contattaci</h1>
+    <!--Form Contatti-->
+    <form novalidate>
+        <!--Email-->
+        <div class="mb-3">
+            <label for="email" class="form-label">La tua email<sup class="text-danger">*</sup></label>
+            <input type="email" class="form-control" id="email" placeholder="name@example.com">
+            <small class="form-text text-muted">La tua mail,ti ricontatteremo a questo indirizzo</small>
+        </div>
+
+        <!--Oggetto-->
+        <div class="mb-3">
+            <label for="subject" class="form-label">Oggetto della mail<sup class="text-danger">*</sup></label>
+            <input type="text" class="form-control" id="subject" required>
+        </div>
+
+        <!--Messaggio-->
+        <div class="mb-3">
+            <label for="message" class="form-label">Contenuto della mail<sup class="text-danger">*</sup></label>
+            <textarea type="text" class="form-control" id="message" cols="20" rows="10"></textarea>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <button class="btn btn-primary" type="submit">Invia</button>
+        </div>
+
+
+
+
+
+    </form>
 </template>
 
 <style></style>
