31aa058f7639b6f39e125196f27cd93f459e57cf
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-12 19:57:00 +0200

    added component FormAlert

---
+++ src/components/forms/FormAlert.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'FormAlert',
+    props: {
+        isOpen: Boolean,
+        type: String | null,
+        dismissible: Boolean,
+    },
+    emits: ['close']
+}
+
+</script>
+
+<template>
+    <div v-if="isOpen" class="alert fade show"
+        :class="[`alert-${type ?? 'danger'}`, { 'alert-dismissable': dismissible }]" role="alert">
+        <slot></slot>
+        <button v-if="dismissable" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
+            @click="$emit('close')"></button>
+    </div>
+</template>
+
+<style></style>
