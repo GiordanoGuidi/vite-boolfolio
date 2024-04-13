dc67cc72643cdef273b511b351a472cacd40625b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-13 22:37:06 +0200

    filled props

--- src/pages/ContactUsPage.vue
+++ src/pages/ContactUsPage.vue
@@ -1,25 +1,44 @@
 <script>
 import { store } from '../data/store';
 import axios from 'axios';
+import FormAlert from '../components/forms/FormAlert.vue';
 //Endpoint
 const endpoint = 'http://localhost:8000/api/contact-message';
 //Variabile del form con i campi vuoti
 const emptyForm = { email: '', subject: '', message: '', }
 export default {
     name: 'ContactUsPage',
+    components: { FormAlert },
     data: () => ({
         form: emptyForm,
+        successMessage: null,
+        errors: [],
     }),
+    computed: {
+        hasErrors() {
+            return Object.keys(this.errors).length;
+        },
+        showAlert() {
+            //trasformiamo i valori in un booleano
+            return Boolean(this.successMessage || this.hasErrors)
+        },
+
+        alertType() {
+            return this.hasErrors ? 'danger' : 'success';
+        }
+    },
     methods: {
         //Funzione chiamata all'invio del form
         sendForm() {
             store.isLoading = true
             axios.post(endpoint, this.form).then(() => {
                 // Svuotiamo il form
-                this.form = { ...emptyForm }
+                this.form = { ...emptyForm };
+                this.successMessage = 'Il messaggio è stato inviato con successo'
             })
                 .catch(err => {
-                    console.error(err)
+                    console.error(err);
+                    this.errors = { network: 'Si è verificato un errore!' }
                 })
                 .then(() => {
                     store.isLoading = false;
@@ -31,6 +50,9 @@ export default {
 
 <template>
     <h1>Contattaci</h1>
+
+    <!-- TODO gestire alert-->
+    <FormAlert :isOpen="showAlert" :type="alertType" :dismissible="!hasErrors" />
     <!--Form Contatti-->
     <form @submit.prevent="sendForm()" novalidate>
         <!--Email-->
